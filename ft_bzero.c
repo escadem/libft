4d33c4eb6f8b37a4e45823c901ab24a32869b26c
Enrique Escat Lloveras <eescat-l@c3r4s1.42madrid.com>
2022-09-30 21:08:25 +0200

    ft_bzero corregido

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: eescat-l <eescat-l@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 22:59:16 by eescat-l          #+#    #+#             */
-/*   Updated: 2022/09/29 13:20:20 by eescat-l         ###   ########.fr       */
+/*   Updated: 2022/09/30 21:07:33 by eescat-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,42 +21,42 @@ void	*ft_bzero(void *s, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		*(unsigned char *) (s + i) = (unsigned char) '0';
+		*(unsigned char *) (s + i) = (unsigned char) 0;
 		i ++;
 	}
 	return(s);
 }
 
-int main(void)
-{
-	char str[] = "Hola que tal";
-	printf("cadena: %s\n", str);
-	ft_bzero(str, 5);
-	printf("cadena: %s\n", str);
+// int main(void)
+// {
+// 	char str[] = "Hola que tal";
+// 	printf("cadena: %s\n", str);
+// 	ft_bzero(str, 5);
+// 	printf("cadena: %s\n", str);
 
-	int i;
-	int n;
-	int array[5] = {12, 45, 75, 98, 01};
+// 	int i;
+// 	int n;
+// 	int array[5] = {12, 45, 75, 98, 01};
 	
-	i = 0;
-	printf("[");
-	while (i < 5)
-	{
-		printf("%i ", array[i]);
-		i ++;
-	}
-	printf("]\n");
+// 	i = 0;
+// 	printf("[");
+// 	while (i < 5)
+// 	{
+// 		printf("%i ", array[i]);
+// 		i ++;
+// 	}
+// 	printf("]\n");
 
-	i = 0;
-	n = 4;
+// 	i = 0;
+// 	n = 8;
 
-	ft_bzero(&array[0], n);
+// 	ft_bzero(&array[0], n);
 	
-	printf("[");
-	while (i < 5)
-	{
-		printf("%i ", array[i]);
-		i ++;
-	}
-	printf("]\n");
-}
+// 	printf("[");
+// 	while (i < 5)
+// 	{
+// 		printf("%i ", array[i]);
+// 		i ++;
+// 	}
+// 	printf("]\n");
+// }
