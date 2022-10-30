e1f6677f3d43327d8788198cc8f95b5994e1a437
Enrique Home <escat.academ@hotmail.com>
2022-10-30 17:18:27 +0100

    ft_strtrim & ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,52 +6,163 @@
 /*   By: eescat-l <eescat-l@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 12:57:59 by eescat-l          #+#    #+#             */
-/*   Updated: 2022/10/11 13:09:37 by eescat-l         ###   ########.fr       */
+/*   Updated: 2022/10/30 17:15:27 by eescat-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
+// size_t	ft_strlen(const char *s)
+// {
+// 	size_t	i;
 
-	i = 0;
-	while (s[i])
-		i ++;
-	return (i);
+// 	i = 0;
+// 	while (s[i])
+// 		i ++;
+// 	return (i);
+// }
+
+// void	*ft_calloc(size_t count, size_t size)
+// {
+// 	void	*p;
+
+// 	if (!count || !size)
+// 		return (NULL);
+// 	if (count > SIZE_MAX / size)
+// 		return (NULL);
+// 	p = (void *)malloc(count * size);
+// 	if (p == NULL )
+// 		return (NULL);
+// 	return (ft_bzero(p, count * size));
+// }
+
+// void	*ft_bzero(void *s, size_t n)
+// {
+// 	size_t	i;
+
+// 	if (!s)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < n)
+// 	{
+// 		*(unsigned char *)(s + i) = (unsigned char) 0;
+// 		i ++;
+// 	}
+// 	return (s);
+// }
+
+static size_t	ft_count_rows(char const *s, char c)
+{
+	int	rows;
+	int	index;
+
+	index = 0;
+	rows = 0;
+	while (*s)
+	{
+		if (*s != c && index == 0)
+		{
+			index = 1;
+			rows ++;
+		}
+		else if (*s == c)
+			index = 0;
+		s++;
+	}
+	return (rows + 1);
 }
 
-static int	ft_count_c(char const *s, char c)
+static void	ft_count_rowlen(char const *s, char c, size_t *vect)
 {
-	int	i;
-	int	counter;
+	int		i;
+	int		j;
+	size_t	length;
 
 	i = 0;
-	counter = 0;
-	while (s[i])
+	j = 0;
+	while (s[i + j])
 	{
-		if (s[i] == c)
-			counter ++;
-		i ++;
+		length = 0;
+		while (s[i + j] != c)
+		{
+			length ++;
+			j++;
+		}
+		if (length != 0)
+		{
+			vect[i] = length + 1;
+			i++;
+		}
+		else if (length == 0)
+			j++;
 	}
+	vect[i] = 0;
+}
+
+static char	**ft_split_exe(size_t *vect, char **str, char const *s, char c)
+{
+	size_t	row;
+	size_t	s_counter;
+	size_t	i;
+
+	row = 0;
+	s_counter = 0;
+	i = 0;
+	while (vect[row])
+	{
+		str[row] = ft_calloc((vect[row] + 1), sizeof(char));
+		s_counter = 0;
+		while (s_counter < vect[row] - 1)
+		{
+			if (*(s + i) != c)
+			{
+//printf("\trow = %zu\t\ts + %zu = %c\n",row, i, *(s + i));
+				str[row][s_counter] = *(s + i);
+				s_counter++;
+			}
+			i++;
+		}
+		row++;
+	}
+	str[row] = ft_calloc(1, sizeof(char));
+	return (str);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int	*vect;
+	size_t	*vect;
+	char	**str;
+	size_t	i;
 
-	vect = ft_calloc(ft_strlen(s), sizeof(int));
-	if (vect)
+	if (*s == 0)
 		return (NULL);
-	vect = ft_
+	i = ft_count_rows(s, c);
+	vect = ft_calloc(i, sizeof(size_t));
+	ft_count_rowlen(s, c, vect);
+// printf("i = %zu\n", i);
+// for (int j=0; j< (int)i; j++)
+// printf("vect[%d] = %zu \n",j, vect[j]);
+	str = ft_calloc((i + 1), sizeof(char *));
+// printf("calloc str OK\n");	
+	str = ft_split_exe(vect, str, s, c);
+// printf("ft_split_exe OK\n");	
+	free (vect);
+	return (str);
 }
 
 // int main(void)
 // {
 // 	char *s1 = "1A2B3C4A5E6G7D8F9HHH0A";
-// 	char c = "ABCDEFGH";
+// 	char c = 'A';
+// 	int i = 0;
 // 	char **s3;
-// 	s3 = ft_strtrim(s1, s2);
-// 	printf("s3: %s\n", s3);
+
+// 	s3 = ft_split(s1, c);
+// printf("ft_split OK\n");
+// 	while (**(s3 + i))
+// 	{
+// 		printf ("i = %d, \nrow %d: %s\n", i, i, *(s3 + i));
+// 		i++;
+// 	}
+// 	return (0);
 // }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,30 +6,20 @@
 /*   By: eescat-l <eescat-l@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 19:01:17 by eescat-l          #+#    #+#             */
-/*   Updated: 2022/10/16 23:09:34 by eescat-l         ###   ########.fr       */
+/*   Updated: 2022/10/21 21:21:12 by eescat-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (s[i])
-// 		i ++;
-// 	return (i);
-// }
-
-static int	ft_chletter(char c, char const *set)
+static int	ft_check_letter(char c, char const *set)
 {
 	int	i;
 
 	i = 0;
 	while (set[i])
 	{
-		if (c == set[i])
+		if (set[i] == c)
 			return (1);
 		i ++;
 	}
@@ -40,32 +30,34 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
 	size_t	i;
-	size_t	len;
-	int		count;
+	size_t	count;
+	size_t	pos;
 
 	i = 0;
 	count = 0;
-	len = ft_strlen(s1);
-	str = (char *) malloc (len * sizeof(char));
+	pos = 0;
+	while (*(s1 + count) && ft_check_letter(*(s1 + count), set))
+		count++;
+	str = (char *) malloc ((count + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
 	{
-		if (ft_chletter(s1[i], set) == 0)
+		if (ft_check_letter(s1[i], set) == 0)
 		{
-			str[count] = s1[i];
-			count ++;
+			str[pos] = s1[i];
+			pos ++;
 		}
 		i ++;
 	}
-	str[count] = '\0';
+	str[pos] = '\0';
 	return (str);
 }
 
 // int main(void)
 // {
 // 	char *s1 = "1A2B3C4A5E6G7D8F9HHH0A";
-// 	char *s2 = "";
+// 	char *s2 = "ABCDEFGH";
 // 	char *s3;
 // 	s3 = ft_strtrim(s1, s2);
 // 	printf("s3: %s\n", s3);
