1c29a21b7f1c83e89678a395e1bfd8e57036ab9e
Enrique Escat Lloveras <eescat-l@c2r11s6.42madrid.com>
2022-12-03 12:23:15 +0100

    ft_lstmap solucionado problemas de alocacion de memoria

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eescat-l <eescat-l@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 19:38:12 by eescat-l          #+#    #+#             */
-/*   Updated: 2022/11/11 20:38:38 by eescat-l         ###   ########.fr       */
+/*   Updated: 2022/12/03 12:14:43 by eescat-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	while (lst)
 	{
 		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&newlst, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&newlst, temp);
 		lst = lst->next;
 	}
